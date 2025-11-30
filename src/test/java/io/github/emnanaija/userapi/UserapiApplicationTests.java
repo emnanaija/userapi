0ae62ea868a6cd9ee43cc91562f9a4e8fa0a9e0d
emna-naija-pro <emnanaija@gmail.com>
2025-11-30 14:43:59 +0100

    Initial commit: Spring Boot project setup with Web, JPA, H2, Validation, Lombok

---
+++ src/test/java/io/github/emnanaija/userapi/UserapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.emnanaija.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
